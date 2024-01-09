e62955e75c9a491c658e82d3101ed86587b5f0d5
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2024-01-09 02:23:22 -0500

    parse env

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,39 +1,40 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import "express-async-errors";
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { Server as SocketServer } from "socket.io";
-import { createServer } from "./lib/http-server";
-import { errorHandler } from "./middleware/error-handler";
-import { DockerService } from "./lib/v3/docker/docker-service";
-import { SocketIO } from "./lib/socket-io";
-import { minecraftCreateRouter } from "./routers/v3/minecraft-servers/create";
-import { NotFoundError } from "./lib/exceptions/not-found-error";
-import { minecraftStartRouter } from "./routers/v3/minecraft-servers/start";
+import 'express-async-errors';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { Server as SocketServer } from 'socket.io';
+import { createServer } from './lib/http-server';
+import { errorHandler } from './middleware/error-handler';
+import { DockerService } from './lib/v3/docker/docker-service';
+import { SocketIO } from './lib/socket-io';
+import { minecraftCreateRouter } from './routers/v3/minecraft-servers/create';
+import { NotFoundError } from './lib/exceptions/not-found-error';
+import { minecraftStartRouter } from './routers/v3/minecraft-servers/start';
+import { env } from './lib/env';
 
 const app = express();
 const server = createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN,
+    origin: env.CORS_ORIGIN,
   },
 });
 
-const docker = new DockerService(process.env.CONTAINERS_DIR!, new SocketIO(io));
+const docker = new DockerService(env.CONTAINERS_DIR, new SocketIO(io));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: env.CORS_ORIGIN,
     credentials: true,
-    allowedHeaders: ["Cookie", "Content-Type"],
+    allowedHeaders: ['Cookie', 'Content-Type'],
   })
 );
 
-app.use("/api/", [
+app.use('/api/', [
   // minecraftGetOneRouter,
   // minecraftGetAllRouter,
   minecraftCreateRouter,
@@ -44,7 +45,7 @@ app.use("/api/", [
   // minecraftRestartRouter,
 ]);
 
-app.all("*", async () => {
+app.all('*', async () => {
   throw new NotFoundError();
 });
 
---
+++ api/src/lib/env.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const env = {
+  CONTAINERS_DIR: z.string().min(1).parse(process.env.CONTAINERS_DIR),
+  NEXTAUTH_SECRET: z.string().min(1).parse(process.env.NEXTAUTH_SECRET),
+  CORS_ORIGIN: z.string().min(1).parse(process.env.CORS_ORIGIN),
+};
